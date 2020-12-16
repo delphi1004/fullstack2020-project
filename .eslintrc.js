e608840b00f2b882631dbf7ab141ed716d8fc9d2
delphi1004 <delphi1004@hanmail.net>
2020-12-17 05:58:18 +0900

    added

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,10 +22,7 @@ module.exports = {
   ],
   rules: {
     indent: ['error', 2, { SwitchCase: 1 }],
-    'linebreak-style': [
-      'error',
-      'windows'
-    ],
+    'linebreak-style': 0,
     quotes: [
       'error',
       'single'
